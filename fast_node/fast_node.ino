321d37fa70b6c62bcae6dc833347de773c45248f
Mike Zero <m0@pisem.net>
2016-04-05 19:02:26 +0300

    fast_node.ino: digital read added

--- fast_node/fast_node.ino
+++ fast_node/fast_node.ino
@@ -1,16 +1,22 @@
 /* fast measuring sensor node */
 
 #define LED 13
-#define analog_pins 8
 
-// for analog_data
+#define analog_pins 8
+#define digital_pins 14
+
+// for analog_data and digital_data
 #define DATA_LAST 0
-#define DATA_MIN 1
-#define DATA_MAX 2
-#define DATA_CHANGES 3
+#define DATA_CHANGES 1
+#define DATA_MIN 2
+#define DATA_MAX 3
+
+#define ANALOG_DATA_STATES 4	// last, changes, min, max
+#define DIGITAL_DATA_STATES 2	// last, changes
 
 // for analog_data_big
 #define DATA_SUM 0
+#define ANALOG_DATA_BIG_STATES 1
 
 uint16_t report_period_millis = 5 * 1000;
 uint16_t fast_measures_per_report = 1000;
@@ -24,11 +30,15 @@ unsigned long slow_measure_period = slow_measures_per_report > 0 ? report_period
 signed long wait_millis;
 signed long wait_millis_temp;
 
-uint16_t analog_mask = 0x0001;
-uint16_t analog_data[analog_pins][4];	// states - last, min, max, changes
-unsigned long analog_data_big[analog_pins][1];	// states for longs - sum
+uint16_t analog_mask = 0x0003;
+uint16_t analog_data[analog_pins][ANALOG_DATA_STATES];
+unsigned long analog_data_big[analog_pins][1];
+
+uint16_t digital_mask = 0x0004;
+uint16_t digital_data[digital_pins][DIGITAL_DATA_STATES];
 
 uint16_t mask = 0;
+uint8_t pin = 0;
 uint16_t count_fast = 0;
 uint16_t count_slow = 0;
 
@@ -82,14 +92,16 @@ void loop() {
 
 void report() {
 	next_report_time += report_period_millis;
-	Serial.print(millis());
-	Serial.println(" Report");
+	Serial.print(" count_fast=");
+	Serial.print(count_fast);
+	Serial.print(" count_slow=");
+	Serial.println(count_slow);
 
 	mask = analog_mask;
-	uint8_t pin = 0;
+	pin = 0;
 	while (mask > 0) {
 		if ((mask & 1) == 1) {
-			Serial.print("A");
+			Serial.print(" A");
 			Serial.print(pin);
 			Serial.print(": ");
 			Serial.print(analog_data[pin][DATA_MIN]);
@@ -101,8 +113,9 @@ void report() {
 			Serial.print(analog_data_big[pin][DATA_SUM]);
 			Serial.print(" chgs: ");
 			Serial.print(analog_data[pin][DATA_CHANGES]);
+			// just for debug
 			Serial.print(" avg: ");
-			Serial.print(analog_data_big[pin][DATA_SUM] / analog_data[pin][DATA_CHANGES]);
+			Serial.print(analog_data_big[pin][DATA_SUM] / count_fast);
 
 			analog_data[pin][DATA_MIN] = 0;
 			analog_data[pin][DATA_MAX] = 0;
@@ -113,7 +126,26 @@ void report() {
 		pin++;
 		mask >>= 1;
 	}
-//	Serial.println();
+
+	mask = digital_mask;
+	pin = 0;
+	while (mask > 0) {
+		if ((mask & 1) == 1) {
+			Serial.print(" D");
+			Serial.print(pin);
+			Serial.print(": ");
+			Serial.print(digital_data[pin][DATA_LAST]);
+			Serial.print(" chgs: ");
+			Serial.print(digital_data[pin][DATA_CHANGES]);
+
+			digital_data[pin][DATA_CHANGES] = 0;
+			Serial.println();
+		}
+		pin++;
+		mask >>= 1;
+	}
+
+	Serial.println();
 	count_fast = 0;
 	count_slow = 0;
 }
@@ -132,6 +164,17 @@ void fast_measure() {
 			if (count_fast == 0 || val < analog_data[pin][DATA_MIN]) analog_data[pin][DATA_MIN] = val;
 			if (count_fast == 0 || val > analog_data[pin][DATA_MAX]) analog_data[pin][DATA_MAX] = val;
 			analog_data[pin][DATA_LAST] = val;
+		}
+		pin++;
+		mask >>= 1;
+	}
+	mask = digital_mask;
+	pin = 0;
+	while (mask > 0) {
+		if ((mask & 1) == 1) {
+			int val = digitalRead(pin);
+			if (val != digital_data[pin][DATA_LAST]) digital_data[pin][DATA_CHANGES]++;
+			digital_data[pin][DATA_LAST] = val;
 		}
 		pin++;
 		mask >>= 1;
